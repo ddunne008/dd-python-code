74dd4723c4f6febf310364b94e0c5bbf18ed18dc
ddunne008 <d.dunne2447@student.leedsbeckett.ac.uk>
2024-10-19 13:54:56 +0100

    19/10/2024

--- practicals/00-first-programs/test.py
+++ practicals/00-first-programs/test.py
@@ -1,3 +1,8 @@
-F = input("Enter the Fahrenheit temperature: ")
-C = F * (9/5) + 32
-print(C, "Celsius")
+A = input("Enter a number for A: ")
+B = input("Enter a number for B: ")
+Sum = A + B
+Product = A * B
+
+print("The value of A is", A,"and the value of B is", B)
+print("The sum of", A ,"and", B,"is ",Sum)
+print("The product of", A ,"and", B,"is ",Product)
