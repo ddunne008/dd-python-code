2c5f47dd11118a086dfe9dcc5923e422c432b1a2
ddunne008 <d.dunne2447@student.leedsbeckett.ac.uk>
2024-10-21 19:15:41 +0100

    21/10/2024

--- practicals/03-control-statements/03-Q7.py
+++ practicals/03-control-statements/03-Q7.py
@@ -2,7 +2,13 @@ times_table = int(input("Enter the times table number: "))
 
 
 if times_table <= 0 or times_table <= 12:
-    for a in range(13):
-        print(f"{a} * {times_table} = {a * times_table}")
+    times_table = abs(times_table)
+
+    for b in range(12, -1, -1):
+        print(f"{b} * {times_table} = {b * times_table}")
 else:
-    print("Please enter a number between 0 or 12")
+    if 0 <= times_table <= 12:
+        for b in range(13):
+            print(f"{b} * {times_table} = {b * times_table}")
+    else:
+        print("Please enter a number between 0 or 12")
