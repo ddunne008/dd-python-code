809cc5828ca358b8645af9cb07bf5278de3d87b4
Dan <D.Dunne2447@student.leedsbeckett.ac.uk>
2024-10-24 15:43:34 +0100

    24/10/2024

---
+++ practicals/04-defining-functions/04-Q1.py
@@ -0,0 +1,11 @@
+def printHeader(msg):
+    print("*****", msg)
+
+printHeader()
+
+
+printHeader("Hello there!")
+printHeader("Welcome")
+printHeader("To")
+printHeader("The")
+printHeader("System!")
