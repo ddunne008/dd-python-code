71febecb5946dd2e664b782461bf22d799641b32
ddunne008 <d.dunne2447@student.leedsbeckett.ac.uk>
2024-11-09 02:16:05 +0000

    Project Update

---
+++ practicals/99-additional-work/Project_2_Chatbot.py
@@ -0,0 +1,16 @@
+import random
+
+#The Agents list contains a list of names who are representing as Agents within the Chatbot system
+Agents = ["Martha", "Sarah","Dean","Alex","Joseph","Dylan"]
+
+def Close_program(exit)
+    if terminal == "exit" or "quit" or "leave" or "stop"
+
+
+
+#This will be the first part of the chatbot system which will display a welcome to the chatbot
+print("****Welcome to the University of Poppleton Chatbot System!*****")
+username = input("What is your name: ")
+print("Hello", username,"Good to meet you, give me one moment whilst I connect you to a Agent")
+
+terminal = input("Hello,")
