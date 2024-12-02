e910119c5255623f20e3643fef48edb13bb51756
ddunne008 <d.dunne2447@student.leedsbeckett.ac.uk>
2024-12-02 16:46:58 +0000

    Project

--- project/Project_2_Chatbot.py
+++ project/Project_2_Chatbot.py
@@ -4,7 +4,7 @@ import random
 Agents = ["Martha", "Sarah","Dean","Alex","Joseph","Dylan", "Carlos", "Amelia", "Eden"]
 
 def Close_program(exit):
-    if terminal == "exit" or "quit" or "leave" or "stop"
+    if terminal == "exit" or "quit" or "leave" or "stop":
         quit()
 
 
@@ -14,5 +14,5 @@ print("****Welcome to the University of Poppleton Chatbot System!*****")
 username = input("What is your name: ")
 print("Hello", username,"Good to meet you, give me one moment whilst I connect you to an agent")
 
-
-terminal = input("Hello", username,"Agent", agent, "is connected")
+agent = random.choice(Agents)
+terminal = input("Hello", username,"Agent", agent,"is connected")
