244489deb45bcb71dc4e4e25618bc700b5a4e3f5
Dan <D.Dunne2447@student.leedsbeckett.ac.uk>
2025-01-09 13:56:10 +0000

    Last Commit

--- exam/Miles_per_Consumption.py
+++ exam/Miles_per_Consumption.py
@@ -1,5 +1,4 @@
-import sys # I believe that for the file to be accessed it would use the Import sys module and then to allow command line arguments, sys.argv
-# This code file is a WIP. Unfortuntely could not program it to read the external file so I have added the contents as a list called 'data'
+import sys
 data = [57.4, 62.3, 58.9, 48.9, 55.0]
 
 m = max(data)
@@ -8,3 +7,7 @@ av = sum(data) / len(data)
 print(f"The Largest number of miles per gallon consumption is {m}")
 print(f"the Average number of miles per gallon consumption is {av}")
 
+# This Python program reads a list of data and then calculates the maximum Miles per gallon value and the average consumption of fuel
+
+# This code file is a WIP. Unfortuntely could not program it to read the external file so I have added the contents as a list called 'data' instead
+# I believe that for the file to be accessed it would possible involve Import sys and the sys.argv so it can read command line arguments
