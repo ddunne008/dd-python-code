690d7aea90dfab72f95918921800ef77292a3c41
Dan <D.Dunne2447@student.leedsbeckett.ac.uk>
2025-01-09 13:11:06 +0000

    Third Commit

---
+++ exam/Speed_coversion_table.py
@@ -0,0 +1,9 @@
+print("Conversion Table of Miles to Kilometers")
+
+print("Handy Speed Converter Table MPH to KM")
+
+
+for a in range(0, 75, 5):
+    a = float(a)
+    print(a, "mph is equal to", a * 1.609, "km/h")
+
